9fee56b61219c6335e5acf4b0d59cfc61d039d25
Oksana Tsivileva <59922105+oxanatsivileva@users.noreply.github.com>
2023-07-09 17:17:24 +0500

    init

---
+++ src/pages/PostPageCompositionApi.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+        <h1>Страница с постами</h1>
+        <my-input
+            v-model="searchQuery"
+            placeholder="Поиск..."
+            v-focus
+        />
+        <div class="app__btns">
+            <my-button
+            >
+                Создать пост
+            </my-button>
+            <my-select 
+            v-model="selectedSort"
+            :options="sortOptions"
+            
+            ></my-select>
+        </div>
+        <my-dialog v-model:show="dialogVisible">
+            <post-form
+            />
+        </my-dialog>
+        <post-list 
+            :posts="sortedAndSearchedPosts"
+            v-if="!isPostsLoading"
+        />
+        <div v-else>Идет загрузка...</div>
+    </div>
+
+</template>
+
+
+<script>
+import postForm from "@/components/postForm.vue";
+import postList from "@/components/postList.vue";
+import axios from "axios";
+import {ref} from 'vue';
+import {usePosts} from "@/hooks/usePosts";
+import useSortedPosts from "@/hooks/useSortedPosts";
+import useSortedAndSearchedPosts from "@/hooks/useSortedAndSearchedPosts";
+
+
+
+    export default {
+        components: {
+           postForm, postList
+        },
+        data() {
+            return {
+                dialogVisible: false,
+                sortOptions: [
+                    {value: 'title', name: 'По названию'},
+                    {value: 'body', name: 'По содержанию'},
+                ]
+            }
+        },
+        setup(props) {
+            const {posts, totalPages, isPostsLoading} = usePosts(10);
+            const {sortedPosts, selectedSort} = useSortedPosts(posts);
+            const {searchQuery, sortedAndSearchedPosts} = useSortedAndSearchedPosts(sortedPosts);
+
+            return {
+                posts,
+                totalPages,
+                isPostsLoading,
+                sortedPosts,
+                selectedSort,
+                searchQuery,
+                sortedAndSearchedPosts
+            }
+
+        }
+    }
+</script>
+
+
+<style>
+
+
+.app__btns {
+    display: flex;
+    justify-content: space-between;
+    margin: 15px 0;
+
+}
+
+.page__wrapper {
+    display: flex;
+    margin-top: 15px;
+}
+
+.page {
+    border: 1px solid black;
+    padding: 10px;
+}
+
+.current-page {
+    border: 2px solid teal;
+}
+
+.observer {
+    height: 30px;
+    background: green;
+}
+</style>
